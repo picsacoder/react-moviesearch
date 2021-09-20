036b937a3a29fd11a31914816d465fa3b794cfa2
PcsaDev <66335345+picsacoder@users.noreply.github.com>
2021-09-20 10:10:19 -0500

    search form, title component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
+import { Title } from './components/titleComponent';
+import { SearchForm } from './components/searchComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Search Movies</Title>
+
+      <div className="SearchForm-wrapper">
+        <SearchForm/>
+      </div>
     </div>
   );
 }
